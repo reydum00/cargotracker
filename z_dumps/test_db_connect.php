9e591dcc2b7c3b5ed2b782a623359a2f8d62a7f9
reydum00 <reydum00@gmail.com>
2026-04-10 19:50:11 +0800

    Delete z_dumps directory

--- z_dumps/test_db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db_connect.php';
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully to database!";
-}
-?>
