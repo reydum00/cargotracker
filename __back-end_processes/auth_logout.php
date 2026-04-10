1dce832ce0e6eee364b3f2723fb06ecc6c8f7901
reydum00 <reydum00@gmail.com>
2026-04-10 19:49:19 +0800

    Delete __back-end_processes directory

--- __back-end_processes/auth_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-setcookie(session_name(), '', time() - 3600, '/');
-header("Location: ../_user_interface/user_signup.php");
-exit();
-?>
